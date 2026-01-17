efa44cc0c339c1148c2d3db0b441576d72cbcf32
lyhADT <lyh_adt@qq.com>
2026-01-17 14:32:05 +0800

    publish(fitness-timer): init

--- fitness-timer/vite.config.js
+++ fitness-timer/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fitness-timer/',
   build: {
     outDir: '../docs/fitness-timer',
     emptyOutDir: true,

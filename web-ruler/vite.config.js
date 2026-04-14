84bd13b52d8829126d90078d697719facc7a0880
lyhADT <lyh_adt@qq.com>
2026-04-14 15:19:47 +0800

    fix: correct web-ruler base path to /web-ruler/

--- web-ruler/vite.config.js
+++ web-ruler/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/docs/web-ruler/',
+  base: '/web-ruler/',
   build: {
     outDir: '../docs/web-ruler',
     emptyOutDir: true,
